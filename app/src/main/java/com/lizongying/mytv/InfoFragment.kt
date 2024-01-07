0bfac7d6795bacf5f094ba45d97ff12c1736f094
Li ZongYing <lizongying@msn.com>
2024-01-07 21:37:51 +0800

    cctv8k

--- app/src/main/java/com/lizongying/mytv/InfoFragment.kt
+++ app/src/main/java/com/lizongying/mytv/InfoFragment.kt
@@ -6,10 +6,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import androidx.core.content.ContextCompat
-import androidx.core.view.marginBottom
-import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.lizongying.mytv.databinding.InfoBinding
--- app/src/main/java/com/lizongying/mytv/MainFragment.kt
+++ app/src/main/java/com/lizongying/mytv/MainFragment.kt
@@ -209,7 +209,7 @@ class MainFragment : BrowseSupportFragment() {
         ready++
         Log.i(TAG, "ready $ready")
         if (ready == 3) {
-            request.fetchPage()
+//            request.fetchPage()
             val tvViewModel = tvListViewModel.getTVViewModel(itemPosition)
             tvViewModel?.changed()
 
